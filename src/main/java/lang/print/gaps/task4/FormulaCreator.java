31c8efff71ecd9c3e6db73f890804309dab9e957
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-26 16:39:54 +0500

    primitives

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,9 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        double a=0;
-        double b=0;
+        double a;
+        double b;
 
-        System.out.println((9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2));
+
     }
 }
